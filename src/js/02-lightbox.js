0e21a576dcdd7b05743fcb95de70a6885d70d6da
Mykhailo Kutsyi <91030243+MykhailoKutsyi@users.noreply.github.com>
2022-02-06 19:19:53 +0200

    Update 02-lightbox.js

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,9 +1,6 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
-
-
 // Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
 
 const imagesGallery = document.querySelector('.gallery');
@@ -11,16 +8,29 @@ const imagesGallery = document.querySelector('.gallery');
 function createImagesGalleryMarkup(galleryItems) {
     return galleryItems.map(({ preview, original, description }) => {
         return `
-        <a class="gallery__item" href="${original}">
-            <img 
-                class="gallery__image" 
-                src="${preview}" 
-                alt="${description}" 
-            />
-        </a>`
+        <li>
+            <a class="gallery__item" href="${original}">
+                <img 
+                    class="gallery__image" 
+                    src="${preview}" 
+                    alt="${description}" 
+                />
+            </a>
+        </li>`
     }).join('');
 };
 
 const galleryMarkup = createImagesGalleryMarkup(galleryItems);
 
 imagesGallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+
+
+const gallery = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+
+    
+    
